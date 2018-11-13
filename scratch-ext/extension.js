9d552f2bd7bf49d9a22dbb3fe9386ec2086551d9
jursonovicst <github.com@jursonovics.com>
2018-11-13 19:48:26 +0100

    very simple code

---
+++ scratch-ext/extension.js
@@ -0,0 +1,5 @@
+[
+    //{ "name":"Cardplane HW",  "type":"extension", "file":"cardplane-hw-jsext.js",  "md5":"cardplane-hw-thumb.png",  "url":"file:///usr/lib/scratch2/scratch_extensions/cardplanehw.js",  "tags":["hardware"] },
+  //{ "name":"Cardplane HW",  "type":"extension", "file":"cardplane-hw-jsext.js",   "md5":"cardplane-hw-thumb.png",   "url":"file:///usr/lib/scratch2/scratch_extensions/cardplanehw.js",  "tags":["hardware"] },
+    { "name":"Random Wait",   "type":"extension", "file":"cardplane-randomwait.js", "md5":"cardplane-randomwait.png", "url":"file:///usr/lib/scratch2/scratch_extensions/cardplaneapi.js", "tags":["hardware"] }
+]
